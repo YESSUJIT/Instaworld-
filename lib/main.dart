47d50af0c10ed32f14fbd4183d30a3bb7d6ccb61
YESSUJIT <sujit70708090@gmail.com>
2026-09-05 13:00:44 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:gal/gal.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,6 +33,7 @@ class InstaWorldApp extends StatelessWidget {
     );
   }
 }
+
 
 // ------------------------------------------------------------
 // AUTH GATE
